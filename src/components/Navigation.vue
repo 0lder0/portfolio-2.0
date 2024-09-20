218d7335dae858b5fadc66331e4a20efd90eb418
0lder0 <lilii-ann.older@khk.ee>
2024-09-20 23:19:00 +0300

    first commit

---
+++ src/components/Navigation.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+    <nav class="nav-container">
+      <div class="navbar">
+        <ul>
+          <li><a href="#home"><img src="../assets/nav-icons/home.svg"></a></li>
+          <li><a href="#about"><img src="../assets/nav-icons/about.svg"></a></li>
+          <li><a href="#skills"><img src="../assets/nav-icons/skills.svg"></a></li>
+          <li><a href="#projects"><img src="../assets/nav-icons/projects.svg"></a></li>
+          <li><a href="#contact"><img src="../assets/nav-icons/contact.svg"></a></li>
+        </ul>
+      </div>
+    </nav>
+    <nav id="hover-nav-text" class="nav-container">
+      <div class="navbar-text" id="home-text">Home</div>
+      <div class="navbar-text" id="about-text">About Me</div>
+      <div class="navbar-text" id="skills-text">Skills</div>
+      <div class="navbar-text" id="projects-text">Projects</div>
+      <div class="navbar-text" id="contact-text">Contact</div>
+    </nav>
+</template>
+
