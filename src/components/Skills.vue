934f8330c168b81fcef97cbe502c5ebfc9381985
0lder0 <lilii-ann.older@khk.ee>
2024-09-25 01:35:23 +0300

    Skills section, navbar gradient

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -2,5 +2,50 @@
 </script>
 <template>
 
+    <div class="skills-container">
+        <h2>Skills</h2>
+        <div class="skills">
+            <div class="skill">
+                <img src="../assets/icons/html.png" />
+                <p>HTML</p>
+            </div>
+            <div class="skill">
+                <img src="../assets/icons/css.png" />
+                <p>CSS</p>
+            </div>
+            <div class="skill">
+                <img src="../assets/icons/js.png" />
+                <p>JavaScript</p>
+            </div>
+            <div class="skill">
+                <img src="../assets/icons/react.png" />
+                <p>React</p>
+            </div>
+            <div class="skill">
+                <img src="../assets/icons/php.png" />
+                <p>PHP</p>
+            </div>
+            <div class="skill">
+                <img src="../assets/icons/py.png" />
+                <p>Python</p>
+            </div>
+            <div class="skill">
+                <img src="../assets/icons/mysql.png" />
+                <p>MySQL</p>
+            </div>
+            <div class="skill">
+                <img src="../assets/icons/figma.png" />
+                <p>Figma</p>
+            </div>
+            <div class="skill">
+                <img src="../assets/icons/git.png" />
+                <p>Git</p>
+            </div>
+            <div class="skill">
+                <img src="../assets/icons/wordpress.png" />
+                <p>WordPress</p>
+            </div>
+        </div>
+    </div>
 
 </template>
