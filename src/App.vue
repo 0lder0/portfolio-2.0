8a0cda19c2f9da758cacab6dd6d765f863844241
0lder0 <lilii-ann.older@khk.ee>
2024-09-24 19:14:26 +0300

    About Me box

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navigation from './components/Navigation.vue';
 import Intro from './components/Intro.vue';
+import About from './components/About.vue';
 </script>
 
 <template>
   <Navigation />
   <Intro />
+  <About />
 </template>
---
+++ src/components/About.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+<template>
+    <div class="about-container">
+        <div class="text-container">
+            <h2>About Me</h2>
+            <p>
+                I'm a 19-year-old student from Tartu, studying at Tartu's Vocational College. I've been learning coding for 3 years now. I really enjoy working on the frontend of websites, but I also want to improve my skills in backend development. It feels amazing to see something I've created and think about how to make it even better.
+            </p>
+        </div>
+    </div>
+</template>
