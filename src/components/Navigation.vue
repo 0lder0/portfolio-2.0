bc01a785bad2080935b80106da4b9f09c07fff32
0lder0 <liliiannolder@gmail.com>
2025-08-22 00:13:22 +0300

    navbar fixes

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -15,22 +15,34 @@ const hideText = () => {
 
 <template>
     <nav class="nav-container">
-      <div class="navbar">
-        <ul>
-          <li @mouseenter="showText('home')" @mouseleave="hideText"><a href="#home"><img src="../assets/nav-icons/home.svg"></a></li>
-          <li @mouseenter="showText('about')" @mouseleave="hideText"><a href="#about"><img src="../assets/nav-icons/about.svg"></a></li>
-          <li @mouseenter="showText('skills')" @mouseleave="hideText"><a href="#skills"><img src="../assets/nav-icons/skills.svg"></a></li>
-          <li @mouseenter="showText('projects')" @mouseleave="hideText"><a href="#projects"><img src="../assets/nav-icons/projects.svg"></a></li>
-          <li @mouseenter="showText('contact')" @mouseleave="hideText"><a href="#contact"><img src="../assets/nav-icons/contact.svg"></a></li>
-        </ul>
-      </div>
-    </nav>
-    <nav id="hover-nav-text" class="nav-container">
-      <div v-if="hoveredIcon === 'home'" class="navbar-text">Home</div>
-      <div v-if="hoveredIcon === 'about'" class="navbar-text">About Me</div>
-      <div v-if="hoveredIcon === 'skills'" class="navbar-text">Skills</div>
-      <div v-if="hoveredIcon === 'projects'" class="navbar-text">Projects</div>
-      <div v-if="hoveredIcon === 'contact'" class="navbar-text">Contact</div>
-    </nav>
+    <div class="navbar">
+      <ul>
+        <li @mouseenter="showText('home')" @mouseleave="hideText">
+          <a href="#home"><img src="../assets/nav-icons/home.svg" /></a>
+          <div v-if="hoveredIcon === 'home'" class="navbar-text">Home</div>
+        </li>
+
+        <li @mouseenter="showText('about')" @mouseleave="hideText">
+          <a href="#about"><img src="../assets/nav-icons/about.svg" /></a>
+          <div v-if="hoveredIcon === 'about'" class="navbar-text">About Me</div>
+        </li>
+
+        <li @mouseenter="showText('skills')" @mouseleave="hideText">
+          <a href="#skills"><img src="../assets/nav-icons/skills.svg" /></a>
+          <div v-if="hoveredIcon === 'skills'" class="navbar-text">Skills</div>
+        </li>
+
+        <li @mouseenter="showText('projects')" @mouseleave="hideText">
+          <a href="#projects"><img src="../assets/nav-icons/projects.svg" /></a>
+          <div v-if="hoveredIcon === 'projects'" class="navbar-text">Projects</div>
+        </li>
+
+        <li @mouseenter="showText('contact')" @mouseleave="hideText">
+          <a href="#contact"><img src="../assets/nav-icons/contact.svg" /></a>
+          <div v-if="hoveredIcon === 'contact'" class="navbar-text">Contact</div>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
