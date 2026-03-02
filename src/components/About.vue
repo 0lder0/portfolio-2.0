6f94da538ba9ea2c1748b1ec8d117fd2e5b0f3b5
0lder0 <liliiannolder@gmail.com>
2026-03-02 13:53:30 +0200

    age fix

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,7 +3,7 @@
         <div class="text-container">
             <h2>About Me</h2>
             <p>
-                I'm a 20-year-old motivated developer with hands-on experience in frontend frameworks,
+                I'm a 21-year-old motivated developer with hands-on experience in frontend frameworks,
  UI/UX design, WordPress, WooCommerce, and databases. Strong
  problem-solving skills and a good eye for user experience. Quick to learn
  and comfortable working both in a team and independently.
