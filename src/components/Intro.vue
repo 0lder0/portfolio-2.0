bc3d51732d000cdbd2462b471f1ef68962d3ddf3
0lder0 <liliiannolder@gmail.com>
2026-07-08 18:27:43 +0300

    added gif, intro style edits

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,7 +1,17 @@
+<script setup>
+import waveGif from '../assets/images/wave.gif';
+</script>
+
 <template>
-    <div class="intro-container">
-        <h2>Hi, my name is</h2>
-        <h1>Lilii-Ann Older</h1>
-        <h2>and this is my portfolio</h2>
+    <div class="intro-layout">
+        <div class="intro-container">
+            <h2>Hi, my name is</h2>
+            <h1>Lilii-Ann Older</h1>
+            <h2>and this is my portfolio</h2>
+        </div>
+
+        <div class="wave-container" aria-label="Animated wave illustration">
+            <img :src="waveGif" alt="Animated wave" />
+        </div>
     </div>
 </template>
